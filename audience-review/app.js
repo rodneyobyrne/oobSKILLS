927aefa8974ede87c3972d021dfbc0aae1bc15f8
rodneyobyrne <rodneyobyrne@gmail.com>
2026-08-12 18:34:23 -0600

    Load refined audience review experience

--- audience-review/app.js
+++ audience-review/app.js
@@ -1,25 +1,23 @@
 (() => {
-  function loadStylesheet(href) {
+  const loadCSS = href => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = href;
     document.head.appendChild(link);
-  }
+  };
 
-  loadStylesheet('./experience.css?v=1');
-  loadStylesheet('./validation.css?v=1');
+  loadCSS('./experience.css?v=3');
+  loadCSS('./flow-v2.css?v=1');
 
-  function loadScript(src, onload) {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    script.onload = onload;
-    document.body.appendChild(script);
-  }
+  const core = document.createElement('script');
+  core.src = './app-core.js?v=3';
+  core.async = false;
+  core.onload = () => {
+    const flow = document.createElement('script');
+    flow.src = './flow-v2.js?v=1';
+    flow.async = false;
+    document.body.appendChild(flow);
+  };
 
-  loadScript('./app-core.js?v=1', () => {
-    loadScript('./validation.js?v=1', () => {
-      loadScript('./experience.js?v=1');
-    });
-  });
+  document.body.appendChild(core);
 })();
