cfb608edba67f9e4dfc111001811b64e919415a7
rodneyobyrne <rodneyobyrne@gmail.com>
2026-08-12 20:11:39 -0600

    Load Audience Review behavior after static styles

--- audience-review/app.js
+++ audience-review/app.js
@@ -1,20 +1,10 @@
 (() => {
-  const loadCSS = href => {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = href;
-    document.head.appendChild(link);
-  };
-
-  loadCSS('./experience.css?v=3');
-  loadCSS('./flow-v2.css?v=1');
-
   const core = document.createElement('script');
-  core.src = './app-core.js?v=3';
+  core.src = './app-core.js?v=4';
   core.async = false;
   core.onload = () => {
     const flow = document.createElement('script');
-    flow.src = './flow-v2.js?v=1';
+    flow.src = './flow-v2.js?v=2';
     flow.async = false;
     document.body.appendChild(flow);
   };
